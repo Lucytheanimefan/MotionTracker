ac6d2302adc16b917e91e414391e437df3b61f41
Lucy Zhang <spothorse9.lucy@gmail.com>
2018-01-17 11:42:30 -0500

    Stop updating on exit of app

--- MotionTracker/ViewController.swift
+++ MotionTracker/ViewController.swift
@@ -33,13 +33,22 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.chartView.chartDescription?.text = "Gyroscope data"
-        motionUpdates()
-        gyroScopeUpdate()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        motionUpdates()
+        gyroScopeUpdate()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        motionManager.stopGyroUpdates()
+        motionManager.stopDeviceMotionUpdates()
     }
     
     
