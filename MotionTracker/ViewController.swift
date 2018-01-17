cfdf4efe3f68dbd1ac9d6b6f9a66923b4faa3dce
Lucy Zhang <spothorse9.lucy@gmail.com>
2018-01-16 23:27:34 -0500

    CoreMotion experimentation

--- MotionTracker/ViewController.swift
+++ MotionTracker/ViewController.swift
@@ -6,20 +6,50 @@
 //  Copyright © 2018 Lucy Zhang. All rights reserved.
 //
 
+import Foundation
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
+    
+    let motionManager = CMMotionManager()
 
+    @IBOutlet weak var textView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        motionUpdates()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func motionUpdates(){
+        motionManager.startDeviceMotionUpdates(to: OperationQueue.current!) { (deviceMotion, error) in
+            guard error == nil else {
+                return
+            }
+            
+            // do something with device motion
+            self.handleDeviceMotionUpdate(deviceMotion: deviceMotion!)
+        }
+    }
+    func handleDeviceMotionUpdate(deviceMotion:CMDeviceMotion) {
+        let attitude = deviceMotion.attitude
+        let roll = degrees(radians: attitude.roll)
+        let pitch = degrees(radians: attitude.pitch)
+        let yaw = degrees(radians: attitude.yaw)
+        print("Roll: \(roll), Pitch: \(pitch), Yaw: \(yaw)")
+        
+        
+        self.textView.text = "Roll: \(roll), Pitch: \(pitch), Yaw: \(yaw)"
+    }
+    
+    func degrees(radians:Double) -> Double {
+        return 180 / .pi * radians
+    }
 
 }
 
